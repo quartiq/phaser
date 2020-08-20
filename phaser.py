caf80d52d9645cfc559b93d7c9e3ee1593f76f5b
Robert Jördens <rj@quartiq.de>
2020-08-20 16:20:15 +0000

    platform: cleanup

--- phaser.py
+++ phaser.py
@@ -279,12 +279,15 @@ class Platform(XilinxPlatform):
         self.add_platform_command(
                 "set_property INTERNAL_VREF 0.750 [get_iobanks 35]")
         self.toolchain.bitstream_commands.extend([
-            "set_property BITSTREAM.CONFIG.OVERTEMPPOWERDOWN Enable [current_design]",
+            "set_property BITSTREAM.CONFIG.OVERTEMPPOWERDOWN "
+                "Enable [current_design]",
             "set_property BITSTREAM.GENERAL.COMPRESS True [current_design]",
             "set_property BITSTREAM.CONFIG.CONFIGRATE 33 [current_design]",
             "set_property BITSTREAM.CONFIG.SPI_BUSWIDTH 4 [current_design]",
-            "set_property BITSTREAM.CONFIG.USR_ACCESS TIMESTAMP [current_design]",
-            "set_property BITSTREAM.CONFIG.USERID \"{:#010x}\" [current_design]".format(self.userid),
+            "set_property BITSTREAM.CONFIG.USR_ACCESS "
+                "TIMESTAMP [current_design]",
+            "set_property BITSTREAM.CONFIG.USERID "
+                "\"{:#010x}\" [current_design]".format(self.userid),
             "set_property CFGBVS VCCO [current_design]",
             "set_property CONFIG_VOLTAGE 2.5 [current_design]",
         ])
@@ -294,23 +297,35 @@ class Platform(XilinxPlatform):
                 "connect_hw_server",
                 "open_hw_target",
                 "current_hw_device [lindex [get_hw_devices] 0]",
-                # "set_property PROGRAM.FILE {{{build_name}.bit}} [current_hw_device]",
+                # "set_property PROGRAM.FILE {{{build_name}.bit}} "
+                #    "[current_hw_device]",
                 # "program_hw_devices",
                 # "refresh_hw_device",
-                "write_cfgmem -force -format MCS -size 8 -interface SPIx4 -loadbit \"up 0x0 {build_name}.bit\" {build_name}",
-                "create_hw_cfgmem -hw_device [current_hw_device] [lindex [get_cfgmem_parts {{s25fl128sxxxxxx0-spi-x1_x2_x4}}] 0]",
+                "write_cfgmem -force -format MCS -size 8 -interface SPIx4 "
+                    "-loadbit \"up 0x0 {build_name}.bit\" {build_name}",
+                "create_hw_cfgmem -hw_device [current_hw_device] "
+                    "[lindex [get_cfgmem_parts "
+                    "{{s25fl128sxxxxxx0-spi-x1_x2_x4}}] 0]",
                 "set_property PROGRAM.BLANK_CHECK 0 [current_hw_cfgmem]",
                 "set_property PROGRAM.ERASE 1 [current_hw_cfgmem]",
                 "set_property PROGRAM.CFG_PROGRAM 1 [current_hw_cfgmem]",
                 "set_property PROGRAM.VERIFY 1 [current_hw_cfgmem]",
                 "set_property PROGRAM.CHECKSUM 0 [current_hw_cfgmem]",
-                "set_property PROGRAM.ADDRESS_RANGE {{use_file}} [current_hw_cfgmem]",
-                "set_property PROGRAM.FILES {{{build_name}.mcs}} [current_hw_cfgmem]",
-                "set_property PROGRAM.UNUSED_PIN_TERMINATION {{pull-none}} [current_hw_cfgmem]",
-                "create_hw_bitstream -hw_device [current_hw_device] [get_property PROGRAM.HW_CFGMEM_BITFILE [current_hw_device]]",
-                "program_hw_devices"] + ([
+                "set_property PROGRAM.ADDRESS_RANGE "
+                    "{{use_file}} [current_hw_cfgmem]",
+                "set_property PROGRAM.FILES "
+                    "{{{build_name}.mcs}} [current_hw_cfgmem]",
+                "set_property PROGRAM.UNUSED_PIN_TERMINATION "
+                    "{{pull-none}} [current_hw_cfgmem]",
+                "create_hw_bitstream -hw_device [current_hw_device] "
+                    "[get_property PROGRAM.HW_CFGMEM_BITFILE "
+                    "[current_hw_device]]",
+                "program_hw_devices",
+            ] + ([
                     "program_hw_cfgmem",
-                    "boot_hw_device"]
-                    if flash else []) + [
+                    "boot_hw_device -verbose [current_hw_device]"
+            ] if flash else [
+            ]) + [
                 "close_hw_target",
-                "close_hw_manager"])
+                "close_hw_manager"
+            ])
