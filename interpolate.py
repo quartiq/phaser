68671503b71a385d7bce4a472569e9bf6d3b81eb
Robert Jördens <rj@quartiq.de>
2020-09-08 10:55:29 +0000

    interpolate: document a bit, residual cic gain to ciccomp

--- interpolate.py
+++ interpolate.py
@@ -1,4 +1,5 @@
 from migen import *
+from misoc.interconnect.stream import Endpoint
 from misoc.cores.fir import SymMACFIR, HBFMACUpsampler
 from misoc.cores.cic import SuperCIC
 from misoc.cores.duc import complex
@@ -46,21 +47,36 @@ class SampleMux(Module):
 
 class InterpolateChannel(Module):
     def __init__(self):
-        # FIXME: center tap!, add gain 2**7/5**(4-1) in ciccomp, shifts
+        # FIXME: center tap!
+        # ciccomp: cic droop and cic gain, rate 1/10 -> 1/10, gain 2**7/5**3
         self.submodules.ciccomp = SymMACFIR(3)
-        for i, ci in enumerate([263, -5, 1]):
+        for i, ci in enumerate([269, -5, 1]):
             self.ciccomp.coeff.sr[i].reset = ci
+        # hbf1: rate 1/10 -> 1/5, gain=1
         self.submodules.hbf0 = HBFMACUpsampler(
             [-10, 0, 27, 0, -58, 0, 111, 0, -195, 0, 324, 0, -528, 0, 877, 0,
              -1634, 0, 5180, 8192, 5180, 0, -1634,
              0, 877, 0, -528, 0, 324, 0, -195, 0, 111, 0, -58, 0, 27, 0, -10])
+        # hbf1: rate 1/5 -> 2/5, gain=1
         self.submodules.hbf1 = HBFMACUpsampler(
             [-4, 0, 21, 0, -74, 0, 313, 512, 313, 0, -74, 0, 21, 0, -4])
-        self.submodules.cic = SuperCIC(n=4, r=5, width=16)
-        self.input = self.ciccomp.sample.load
-        self.output = self.cic.output
+        # cic: rate 2/5 -> 2/1, gain=5**3
+        self.submodules.cic = SuperCIC(n=4, r=5, width=17)
+        self.input = Endpoint([("data", (14, True))])
+        self.output = Endpoint([("data0", (16, True)), ("data1", (16, True))])
+        # align to msb to save power, 14 bit plus one bit headroom
+        scale = 24 - (14 + 1)
         self.comb += [
+            self.input.connect(self.ciccomp.sample.load, omit=["data"]),
+            self.ciccomp.sample.load.data.eq(self.input.data << scale),
             self.ciccomp.out.connect(self.hbf0.input),
             self.hbf0.output.connect(self.hbf1.input),
-            self.hbf1.output.connect(self.cic.input),
+            self.hbf1.output.connect(self.cic.input, omit=["data"]),
+            self.cic.input.data.eq(self.hbf1.output.data >> scale),
+            self.cic.output.connect(self.output, omit=["data0", "data1"]),
+            # cic gain is r**(n-1) = 5**3, compensate with 2**-7,
+            # the rest (2**7/5**3) is applied by ciccomp
+            # TODO: rounding
+            self.output.data0.eq(self.cic.output.data0 >> 7),
+            self.output.data1.eq(self.cic.output.data1 >> 7),
         ]
---
+++ test_interpolate.py
@@ -0,0 +1,55 @@
+import numpy as np
+import unittest
+
+from migen import *
+
+import interpolate
+
+
+def feed(endpoint, x, rate):
+    n, d = rate
+    t = 0
+    for i, xi in enumerate(x):
+        while t*n < i*d:
+            yield
+            t += 1
+        yield endpoint.data.eq(int(xi))
+        yield endpoint.stb.eq(1)
+        yield
+        t += 1
+        while not (yield endpoint.ack):
+            yield
+        yield endpoint.stb.eq(0)
+
+
+@passive
+def retrieve(endpoint, o):
+    yield
+    while True:
+        yield endpoint.ack.eq(1)
+        yield
+        while not (yield endpoint.stb):
+            yield
+        o.append(((yield endpoint.data0), (yield endpoint.data1)))
+        yield endpoint.ack.eq(0)
+
+
+class TestInter(unittest.TestCase):
+    def setUp(self):
+        self.dut = interpolate.InterpolateChannel()
+
+    def test_init(self):
+        self.assertEqual(len(self.dut.input.data), 14)
+        self.assertEqual(len(self.dut.output.data0), 16)
+
+    def test_seq(self):
+        # impulse response plus latency
+        x = [(1 << 13) - 1] + [0]*(30 + 10)
+        y = []
+        run_simulation(self.dut, [feed(self.dut.input, x, rate=(1, 10)),
+                                  retrieve(self.dut.output, y)],
+                       vcd_name="int.vcd")
+        y = np.ravel(y)
+        print(y)
+        #y0 =
+        #np.testing.assert_equal(y, y0)
