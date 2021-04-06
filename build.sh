ab26575cc3330f6cfeed8a21f5ff224c19d11c0c
Robert Jördens <rj@quartiq.de>
2021-04-06 08:52:15 +0000

    build: robustify

--- build.sh
+++ build.sh
@@ -5,17 +5,15 @@ set -eux
 VIVADO_PREFIX=${VIVADO_PREFIX:-/opt/Xilinx/Vivado}
 VIVADO_LATEST=$(ls $VIVADO_PREFIX | sort -n | tail -1)
 VIVADO=${VIVADO:-$VIVADO_PREFIX/${VIVADO_LATEST:?}}
+export PATH=$PATH:$VIVADO/bin
 
 python3 -m venv --system-site-packages py
+py/bin/pip install -r requirements.txt
+py/bin/python -m pytest
 
-export PATH=$PATH:$PWD/py/bin:$VIVADO/bin
+py/bin/python phaser.py
 
-pip install -r requirements.txt
-
-python -m pytest
-python phaser.py
-
-pip freeze > build/requirements.txt
+py/bin/pip freeze > build/requirements.txt
 tar czvf phaser.tar.gz \
 	build/phaser.bit \
 	build/requirements.txt \
