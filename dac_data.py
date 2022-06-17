3cea39d06804fc864cd28c0349b1e261af32bca9
SingularitySurfer <norman_krackow@gmx.de>
2022-06-17 12:37:15 +0200

    run black

--- dac_data.py
+++ dac_data.py
@@ -16,14 +16,13 @@ def parity(*x):
 # synchronize frame marker to frame processing and to data_sync and thus to
 # ISTR
 
+
 class DacData(Module):
     def __init__(self, pins, swap=((0, 3), (1, 8))):
         self.data_sync = Signal()  # at most every 8 samples (4 sys cycles)
         self.sync_dly = Signal(max=8, reset_less=True)
         # format as in the DS: A0:C0, B0:D0, A1:C1, B1:D1
-        self.data = [[
-            Signal(16, reset_less=True) for _ in range(2)
-            ] for _ in range(4)]
+        self.data = [[Signal(16, reset_less=True) for _ in range(2)] for _ in range(4)]
         self.istr = Signal(reset_less=True)
 
         # buffer for parity calculation
@@ -42,12 +41,14 @@ class DacData(Module):
         self.sync += [
             i.eq(i + 1),
             sync.eq(sync[2:]),
-            If(self.data_sync,
+            If(
+                self.data_sync,
                 i.eq(0),
-                sync.eq(0xf << self.sync_dly),
+                sync.eq(0xF << self.sync_dly),
             ),
             self.istr.eq(0),
-            If(i == 4 - 1,
+            If(
+                i == 4 - 1,
                 self.istr.eq(1),
             ),
         ]
@@ -62,8 +63,7 @@ class DacData(Module):
         self._oserdes([sync[0], sync[0], sync[1], sync[1]], pins.sync_p, pins.sync_n)
 
         # ISTR for write pointer
-        self._oserdes([self.istr, 0, 0, 0],
-                      pins.istr_parityab_p, pins.istr_parityab_n)
+        self._oserdes([self.istr, 0, 0, 0], pins.istr_parityab_p, pins.istr_parityab_n)
 
         # 32 bit parity
         self._oserdes(par, pins.paritycd_p, pins.paritycd_n)
@@ -72,9 +72,9 @@ class DacData(Module):
         # not needed with SYNC+N-div+PLL generated internal OSTR
         # self._oserdes([0, 0, 0, 0], pins.ostr_p, pins.ostr_n)
 
-        for i_port, port in enumerate([
-                (pins.data_a_p, pins.data_a_n),
-                (pins.data_b_p, pins.data_b_n)]):
+        for i_port, port in enumerate(
+            [(pins.data_a_p, pins.data_a_n), (pins.data_b_p, pins.data_b_n)]
+        ):
             for i_pin, pin in enumerate(zip(*port)):
                 bits = [words[i_word][i_port][i_pin] for i_word in range(4)]
                 if (i_port, i_pin) in swap:  # sinara-hw/Phaser#102
@@ -85,15 +85,25 @@ class DacData(Module):
     def _oserdes(self, data, pin_p, pin_n, clk="sys2", attr=set()):
         pin = Signal()
         self.specials += [
-            Instance("OSERDESE2", attr=attr,
-                p_DATA_RATE_OQ="DDR", p_DATA_RATE_TQ="BUF",
-                p_DATA_WIDTH=4, p_TRISTATE_WIDTH=1,
+            Instance(
+                "OSERDESE2",
+                attr=attr,
+                p_DATA_RATE_OQ="DDR",
+                p_DATA_RATE_TQ="BUF",
+                p_DATA_WIDTH=4,
+                p_TRISTATE_WIDTH=1,
                 i_RST=ResetSignal(),
                 i_CLK=ClockSignal(clk),
                 i_CLKDIV=ClockSignal(),
                 # LSB first, D1 is closest to Q
-                i_D1=data[0], i_D2=data[1], i_D3=data[2], i_D4=data[3],
-                i_TCE=1, i_OCE=1, i_T1=0,
-                o_OQ=pin),
+                i_D1=data[0],
+                i_D2=data[1],
+                i_D3=data[2],
+                i_D4=data[3],
+                i_TCE=1,
+                i_OCE=1,
+                i_T1=0,
+                o_OQ=pin,
+            ),
             DifferentialOutput(pin, pin_p, pin_n),
         ]
--- mac_hbf_upsampler.py
+++ mac_hbf_upsampler.py
@@ -20,17 +20,18 @@ class MAC_HBF_Upsampler(Module):
     :param width_coef: Coefficient width (fixed point position)
     :param dsp_arch: DSP block architecture (Xilinx/Lattice)
     """
+
     def __init__(self, coeff, width_d, width_coef, dsp_arch="xilinx"):
 
         assert dsp_arch in ("xilinx", "lattice"), "unsupported dsp architecture"
         self.dsp_arch = dsp_arch
-        n = (len(coeff) + 1)//4
-        if len(coeff) != n*4 - 1:
+        n = (len(coeff) + 1) // 4
+        if len(coeff) != n * 4 - 1:
             raise ValueError("HBF length must be 4*n-1", coeff)
         elif n < 2:
             raise ValueError("Need order n >= 2")
         for i, c in enumerate(coeff):
-            if i == n*2 - 1:
+            if i == n * 2 - 1:
                 if not c:
                     raise ValueError("HBF center tap must not be zero")
             elif i & 1:
@@ -44,26 +45,32 @@ class MAC_HBF_Upsampler(Module):
         dsp_pipelen = 4
         bias = (1 << width_coef - 1) - 1
         coef = []
-        for i, c in enumerate(coeff[: (len(coeff) + 1) // 2: 2]):
+        for i, c in enumerate(coeff[: (len(coeff) + 1) // 2 : 2]):
             coef.append(Signal((width_coef + 1, True), reset_less=True, reset=c))
 
         self.input = Endpoint([("data", (width_d, True))])
         self.output = Endpoint([("data", (width_d, True))])
 
-        x = [Signal((width_d, True), reset_less=True) for _ in range((len(coef) * 2))]  # input hbf
+        x = [
+            Signal((width_d, True), reset_less=True) for _ in range((len(coef) * 2))
+        ]  # input hbf
 
         self.stop = Signal()  # filter output stall signal
         pos = Signal(int(np.ceil(np.log2(len(coef)))))
         pos_neg = Signal(len(pos) + 1)
 
         self.comb += [
-            self.stop.eq(self.output.stb & ~self.output.ack)  # filter is sensitive to output and ignores input stb
+            self.stop.eq(
+                self.output.stb & ~self.output.ack
+            )  # filter is sensitive to output and ignores input stb
         ]
 
         a, b, c, d, mux_p, p = self._dsp()
 
         self.comb += [
-            pos_neg.eq((len(coef)*2) - 1 - pos),  # position from end of input shift reg
+            pos_neg.eq(
+                (len(coef) * 2) - 1 - pos
+            ),  # position from end of input shift reg
             c.eq(bias),
             a.eq(Array(x)[pos]),
             d.eq(Array(x)[pos_neg]),
@@ -71,37 +78,47 @@ class MAC_HBF_Upsampler(Module):
         ]
 
         self.sync += [
-            If(~self.stop,
-               self.input.ack.eq(0),  # default no in ack
-               self.output.stb.eq(0),  # default no out strobe
-               mux_p.eq(0),  # default accumulate
-               pos.eq(pos+1),
-               If(pos == len(coef) - 1,  # new input sample
-                  pos.eq(0),
-                  Cat(x).eq(Cat(self.input.data, x)),  # shift in new sample
-                  self.input.ack.eq(1),
-                  ),
-               If(pos == dsp_pipelen - 2,
-                  mux_p.eq(1),
-                  ),
-               If(pos == dsp_pipelen - 1,  # new output sample at the end of the dsp pipe
-                  self.output.data.eq(p >> width_coef),
-                  self.output.stb.eq(1),
-                  ),
-               If(pos == (len(coef) // 2) + dsp_pipelen - 1,  # emit trivial sample at halfway computation + pipelen
-                  self.output.data.eq(x[len(coef)]),
-                  self.output.stb.eq(1),
-                  )
-               )
+            If(
+                ~self.stop,
+                self.input.ack.eq(0),  # default no in ack
+                self.output.stb.eq(0),  # default no out strobe
+                mux_p.eq(0),  # default accumulate
+                pos.eq(pos + 1),
+                If(
+                    pos == len(coef) - 1,  # new input sample
+                    pos.eq(0),
+                    Cat(x).eq(Cat(self.input.data, x)),  # shift in new sample
+                    self.input.ack.eq(1),
+                ),
+                If(
+                    pos == dsp_pipelen - 2,
+                    mux_p.eq(1),
+                ),
+                If(
+                    pos
+                    == dsp_pipelen - 1,  # new output sample at the end of the dsp pipe
+                    self.output.data.eq(p >> width_coef),
+                    self.output.stb.eq(1),
+                ),
+                If(
+                    pos
+                    == (len(coef) // 2)
+                    + dsp_pipelen
+                    - 1,  # emit trivial sample at halfway computation + pipelen
+                    self.output.data.eq(x[len(coef)]),
+                    self.output.stb.eq(1),
+                ),
+            )
         ]
 
-        if dsp_pipelen > (len(coef)//2):  # if dsp pipe too long
+        if dsp_pipelen > (len(coef) // 2):  # if dsp pipe too long
             self.sync += [
-                If(pos == ((len(coef)//2) + dsp_pipelen - 1) % (len(coef)//2),
-                   # emit trivial sample at halfway computation + pipelen
-                   self.output.data.eq(x[len(coef) + 1]),
-                   self.output.stb.eq(1),
-                   )
+                If(
+                    pos == ((len(coef) // 2) + dsp_pipelen - 1) % (len(coef) // 2),
+                    # emit trivial sample at halfway computation + pipelen
+                    self.output.data.eq(x[len(coef) + 1]),
+                    self.output.stb.eq(1),
+                )
             ]
 
     def _dsp(self):
@@ -130,14 +147,14 @@ class MAC_HBF_Upsampler(Module):
         b_reg = [Signal.like(b) for _ in range(2)]
 
         self.sync += [
-            If(~self.stop,
-               a_reg.eq(a),
-               Cat(b_reg).eq(Cat(b, b_reg)),
-               d_reg.eq(d),
-               ad.eq(a_reg + d_reg),
-               m.eq(ad * b_reg[-1]),  # b is double piped to be in line with a+d
-               If(~mux_p, p.eq(p + m)
-                  ).Else(p.eq(m + c))
-               )
+            If(
+                ~self.stop,
+                a_reg.eq(a),
+                Cat(b_reg).eq(Cat(b, b_reg)),
+                d_reg.eq(d),
+                ad.eq(a_reg + d_reg),
+                m.eq(ad * b_reg[-1]),  # b is double piped to be in line with a+d
+                If(~mux_p, p.eq(p + m)).Else(p.eq(m + c)),
+            )
         ]
         return a, b, c, d, mux_p, p
